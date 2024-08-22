807b482a82a8ee02983e77c5fed69023f588bff1
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2024-08-22 15:14:39 -0300

    question for upcoming programing

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+import { globalCss } from '@ignite-ui/react'
+
+export const globalStyles = globalCss({
+  '*': {
+    boxsizing: 'border-box',
+    padding: 0,
+    margin: 0,
+  },
+
+  body: {
+  
+  },
+})
